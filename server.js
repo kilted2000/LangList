f75739671f3e71ff5871637bf6608ef052b9b4bc
Richard <richardlwhittington@gmail.com>
2026-08-02 00:14:39 -0500

    fill in langlist array

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 let langList = ['javascript', 'typescript', 'java', 'golang', 'python'];
 
 app.get('/', (req, res) => {
-  res.send()
+  res.send("hello world again")
   document.getElementById('lang').addEventListener("click", langItem)
   function langItem(){
     for(let i = 0;i <= langList.length;i++){
