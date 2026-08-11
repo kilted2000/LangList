ffa2feeec4cd7b0f6d715cfafb65b778b02cc789
Richard <richardlwhittington@gmail.com>
2026-08-11 00:32:41 -0500

    WIP:begin seperating be and fe logic

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-let langList = ['javascript', 'typescript', 'java', 'golang', 'python'];
+let langList = ['javascript', 'typescript', 'java', 'golang', 'python', 'rust'];
 
 document.getElementById('lang').addEventListener("click", langItem)
 function langItem() {
